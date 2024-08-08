80a848bc4482af3ee65bae9c4e3905d767006d9d
Daniher01 <daniher02@gmail.com>
2024-08-08 14:28:20 -0400

    se muestra una parte del modulo de los cursos

---
+++ src/types/cursoTypes.ts
@@ -0,0 +1,15 @@
+// Define una interfaz para los datos de los módulos
+export interface Modulo {
+    NUMERO: string;
+    NOMBRE: string;
+    DESCRIPCION: string;
+  }
+  
+export  interface Curso {
+    NOMBRE: string;
+    CODIGO: string;
+    TIPO: string;
+    HORAS_CRONOLOGICAS: string;
+    COMPETENCIA_DESARROLLAR: string;
+    MODULO: Modulo[];
+  }
