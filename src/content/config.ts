a041ecb58222248e94958d2ab49498e69334c3d9
Daniher01 <daniher02@gmail.com>
2024-08-14 13:26:28 -0400

    se genera el articulo con su estructura en css

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,12 @@
 // 1. Importa las utilidades de `astro:content`
-import { defineCollection, z, reference } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 
 const articulosCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
     pubDate: z.string().transform((str) => new Date(str)),
+    image: z.string().optional(),
   }),
 });
 
