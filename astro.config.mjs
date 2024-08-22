fafe39fcac18a94014c664604915a8a889fd3659
Daniher01 <daniher02@gmail.com>
2024-08-22 13:58:34 -0400

    se instala vercel analytics para guiar la pagina

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
+// // https://astro.build/config
+// export default defineConfig({
+//   // Aquí puedes añadir otras configuraciones si las necesitas
+// });
+
+// ---------------- Adaptacion para vercel
+
 export default defineConfig({
-  // Aquí puedes añadir otras configuraciones si las necesitas
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
