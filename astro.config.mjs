eefcb0337f1c6a228dd20aa91dc1b59370d383cb
Daniher01 <daniher02@gmail.com>
2024-10-02 16:38:44 -0300

    se configura para permitir desplegar en servidores apache

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-
-// // https://astro.build/config
-// export default defineConfig({
-//   // Aquí puedes añadir otras configuraciones si las necesitas
-// });
-
-// ---------------- Adaptacion para vercel
+import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
+  output: 'server',  // Puedes usar "server" o "hybrid" dependiendo de tus necesidades
+  adapter: node({
+    mode: 'standalone',
   }),
 });
