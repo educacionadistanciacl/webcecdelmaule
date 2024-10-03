943b8a5eb6ee651e28f2887a4daac41c0576bc0f
Daniher01 <daniher02@gmail.com>
2024-10-03 16:28:14 -0300

    se configura para permitir archivos estaticos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',  // Puedes usar "server" o "hybrid" dependiendo de tus necesidades
+  output: 'static',  // Puedes usar "server" o "hybrid" dependiendo de tus necesidades
   adapter: node({
     mode: 'standalone',
   }),
 });
+
+// import { defineConfig } from 'astro/config'
+
+// export default defineConfig({
+//   // tus opciones de configuración van aquí...
+//   // https://docs.astro.build/es/reference/configuration-reference/
+// })
